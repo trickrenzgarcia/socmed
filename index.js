205688fe40662b92dce41e76beceee691864b876
trickrenzgarcia <trickrenzgarcia@gmail.com>
2021-12-19 21:14:32 +0800

    last commit

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express')
+const bodyparser = require('body-parser')
+const session = require('express-session')
+const { v4: uuidv4 } = require('uuid')
+
 const app = express()
 
+const port = process.env.PORT || 5000
+
+const router = require('./router')
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.use(session({
+  secret: uuidv4(),
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use('/route', router)
+
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send({data: "this is data."})
+  res.render('base', { title : "Login System"})
 })
 
-app.listen(8080)
+app.listen(port, () => {
+  console.log(`Server is up! listening on http://localhost:${port}`)
+})
